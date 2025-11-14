93fd14aee2cbed87e1f3a4922cd9165d52aee32c
Merinda12345 <merindalesh@gmail.com>
2025-11-14 16:36:23 +0200

    created registerstation payload class

--- src/test/java/PayLoadBuilder/WeatherAPIPayloadBuilder.java
+++ src/test/java/PayLoadBuilder/WeatherAPIPayloadBuilder.java
@@ -1,5 +1,18 @@
 package PayLoadBuilder;
 
+import org.json.simple.JSONObject;
+
 public class WeatherAPIPayloadBuilder {
 
+    public static JSONObject registerPayload(String external_id, String name, String latitude, String longitude, String altitude) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("external_id", external_id);
+        jsonObject.put("name", name);
+        jsonObject.put("latitude", latitude);
+        jsonObject.put("longitude", longitude);
+        jsonObject.put("altitude", altitude);
+
+        return jsonObject;
+
+    }
 }
